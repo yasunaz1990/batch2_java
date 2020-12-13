f3688144335bad33eeb302692a8eb3222bbbd0d9
Nijat Muhtar <yasunaz1990@gmail.com>
2020-12-13 13:47:22 -0500

    went over variable

---
+++ src/VariableDemo.java
@@ -0,0 +1,47 @@
+
+// Class: method container
+// groups related methods
+public class VariableDemo {
+
+    // main: method
+    // method: groups executable lines of code
+    public static void main(String[] args) {
+        // 1. Primitive Data Types
+        // - integer  whole number  -290, 0, 450
+        // - decimal floating point  -290.023, 0.0, 679.023
+        // - character              '@', 'a', 'A', ' ', '#', '1'
+        // - String                  "Full Name"
+        // - Logic ( boolean )       true, false
+
+
+
+
+        // 2. Variable
+        int favoriteNumber = 777;
+        boolean isSnowing = false;
+        char myMathClassGrade = 'A';
+        String reply = "I am fine";
+        double PI = 3.14;
+
+
+        // Print all of these out
+        System.out.print(favoriteNumber);
+        System.out.print("\n");
+        System.out.print("\n");
+        System.out.print(isSnowing);
+        System.out.print("\n");
+        System.out.print("\t");
+        System.out.print(myMathClassGrade);
+        System.out.print("\n");
+        System.out.print("\n");
+        System.out.print(reply);
+
+
+        // Variable naming
+        //------------------------------
+        // 1. describe that data
+        // 2. camelCaseNotation
+        // 3. snake_naming_notation
+
+    }
+}
